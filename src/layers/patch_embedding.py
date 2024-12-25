3a5aa5e81bf6a1f196c7b8d076db252171f0a104
Tomás Ampuero <tom.ampuero.c@gmail.com>
2024-12-25 19:52:04 -0300

    fix: add temporal for x shape

--- src/layers/patch_embedding.py
+++ src/layers/patch_embedding.py
@@ -17,7 +17,7 @@ class PatchEmbed(nn.Module):
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
 
     def forward(self, x):
-        B, C, H, W = x.shape
+        B, C, T, H, W = x.shape
         # FIXME look at relaxing size constraints
         assert H == self.img_size[0] and W == self.img_size[1], \
             f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
